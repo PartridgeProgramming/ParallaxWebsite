63f0a4979fb799cb9029a3ee5bbf762be35a29e1
danaiangelidis <angelidisdanai@gmail.com>
2023-10-12 12:42:17 -0400

    nav added

--- srcipt.js
+++ srcipt.js
@@ -1,4 +1,7 @@
-/* Template JavaScript file */
-windows.onload = () => {
+const toggleNav = () => {
+    document.getElementById("main-menu").classList.remove("hidden");
+}
 
+windows.onload = () => {
+    document.getElementById("burger-menu").onclick = toggleNav;
 };
